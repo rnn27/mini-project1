047a5a4ab1155f1a6d9dca233615342ca9a4d268
rnn27 <raghavendra.nawander@students.iiit.ac.in>
2026-09-13 18:20:12 +0530

    A

--- c-shell/src/parser.c
+++ c-shell/src/parser.c
@@ -3,39 +3,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void init_command(Command *command){
-    command->argv=NULL;
-    command->argc=0;
-    command->argv_capacity=0;
+static void init_command(Command *command)
+{
+    command->argv = NULL;
+    command->argc = 0;
+    command->argv_capacity = 0;
 
-    command->input_redirections=NULL;
-    command->input_redirection_count=0;
-    command->input_redirection_capacity=0;
+    command->input_redirections = NULL;
+    command->input_redirection_count = 0;
+    command->input_redirection_capacity = 0;
 
-    command->output_redirections=NULL;
-    command->output_redirection_count=0;
-    command->output_redirection_capacity=0;
+    command->output_redirections = NULL;
+    command->output_redirection_count = 0;
+    command->output_redirection_capacity = 0;
 }
 
 static void free_redirections(Redirection *redirections,
-                              size_t count){
-    for(size_t i=0; i < count; i++){
+                              size_t count)
+{
+    if (redirections == NULL) {
+        return;
+    }
+
+    for (size_t i = 0; i < count; i++) {
         free(redirections[i].filename);
     }
 
     free(redirections);
 }
 
-static void free_command(Command *command){
-    if(command==NULL){
+static void free_command(Command *command)
+{
+    if (command == NULL) {
         return;
     }
 
-    for(size_t i=0; i < command->argc; i++){
-        free(command->argv[i]);
-    }
+    if (command->argv != NULL) {
+        for (size_t i = 0; i < command->argc; i++) {
+            free(command->argv[i]);
+        }
 
-    free(command->argv);
+        free(command->argv);
+    }
 
     free_redirections(command->input_redirections,
                       command->input_redirection_count);
@@ -46,79 +55,84 @@ static void free_command(Command *command){
     init_command(command);
 }
 
-void free_command_list(CommandList *command_list){
-    if(command_list==NULL){
+void free_command_list(CommandList *command_list)
+{
+    if (command_list == NULL) {
         return;
     }
 
-    for(size_t i=0; i < command_list->count; i++){
-        Pipeline *pipeline=&command_list->pipelines[i];
+    if (command_list->pipelines != NULL) {
+        for (size_t i = 0; i < command_list->count; i++) {
+            Pipeline *pipeline = &command_list->pipelines[i];
 
-        for(size_t j=0; j < pipeline->count; j++){
-            free_command(&pipeline->commands[j]);
+            if (pipeline->commands != NULL) {
+                for (size_t j = 0; j < pipeline->count; j++) {
+                    free_command(&pipeline->commands[j]);
+                }
+
+                free(pipeline->commands);
+            }
         }
 
-        free(pipeline->commands);
+        free(command_list->pipelines);
     }
 
-    free(command_list->pipelines);
-
-    command_list->pipelines=NULL;
-    command_list->count=0;
-    command_list->capacity=0;
+    command_list->pipelines = NULL;
+    command_list->count = 0;
+    command_list->capacity = 0;
 }
 
-static int add_pipeline(CommandList *command_list){
-    if(command_list->count >=command_list->capacity){
-        size_t new_capacity=
-           (command_list->capacity==0)
+static int add_pipeline(CommandList *command_list)
+{
+    if (command_list->count >= command_list->capacity) {
+        size_t new_capacity =
+            (command_list->capacity == 0)
                 ? 2
                 : command_list->capacity * 2;
 
-        Pipeline *new_pipelines=realloc(
-            command_list->pipelines,
-            new_capacity * sizeof(Pipeline)
-        );
+        Pipeline *new_pipelines =
+            realloc(command_list->pipelines,
+                    new_capacity * sizeof(Pipeline));
 
-        if(new_pipelines==NULL){
+        if (new_pipelines == NULL) {
             return 0;
         }
 
-        command_list->pipelines=new_pipelines;
-        command_list->capacity=new_capacity;
+        command_list->pipelines = new_pipelines;
+        command_list->capacity = new_capacity;
     }
 
-    Pipeline *pipeline=
+    Pipeline *pipeline =
         &command_list->pipelines[command_list->count];
 
-    pipeline->commands=NULL;
-    pipeline->count=0;
-    pipeline->capacity=0;
-    pipeline->background=0;
+    pipeline->commands = NULL;
+    pipeline->count = 0;
+    pipeline->capacity = 0;
+    pipeline->background = 0;
 
     command_list->count++;
 
     return 1;
 }
 
-static int add_command(Pipeline *pipeline){
-    if(pipeline->count >=pipeline->capacity){
-        size_t new_capacity=
-           (pipeline->capacity==0)
+static int add_command(Pipeline *pipeline)
+{
+    if (pipeline->count >= pipeline->capacity) {
+        size_t new_capacity =
+            (pipeline->capacity == 0)
                 ? 2
                 : pipeline->capacity * 2;
 
-        Command *new_commands=realloc(
-            pipeline->commands,
-            new_capacity * sizeof(Command)
-        );
+        Command *new_commands =
+            realloc(pipeline->commands,
+                    new_capacity * sizeof(Command));
 
-        if(new_commands==NULL){
+        if (new_commands == NULL) {
             return 0;
         }
 
-        pipeline->commands=new_commands;
-        pipeline->capacity=new_capacity;
+        pipeline->commands = new_commands;
+        pipeline->capacity = new_capacity;
     }
 
     init_command(&pipeline->commands[pipeline->count]);
@@ -128,34 +142,34 @@ static int add_command(Pipeline *pipeline){
 }
 
 static int add_argument(Command *command,
-                        const char *value){
-    if(command->argc + 1 >=command->argv_capacity){
-        size_t new_capacity=
-           (command->argv_capacity==0)
+                        const char *value)
+{
+    if (command->argc + 1 >= command->argv_capacity) {
+        size_t new_capacity =
+            (command->argv_capacity == 0)
                 ? 4
                 : command->argv_capacity * 2;
 
-        char **new_argv=realloc(
-            command->argv,
-            new_capacity * sizeof(char *)
-        );
+        char **new_argv =
+            realloc(command->argv,
+                    new_capacity * sizeof(char *));
 
-        if(new_argv==NULL){
+        if (new_argv == NULL) {
             return 0;
         }
 
-        command->argv=new_argv;
-        command->argv_capacity=new_capacity;
+        command->argv = new_argv;
+        command->argv_capacity = new_capacity;
     }
 
-    command->argv[command->argc]=strdup(value);
+    command->argv[command->argc] = strdup(value);
 
-    if(command->argv[command->argc]==NULL){
+    if (command->argv[command->argc] == NULL) {
         return 0;
     }
 
     command->argc++;
-    command->argv[command->argc]=NULL;
+    command->argv[command->argc] = NULL;
 
     return 1;
 }
@@ -164,40 +178,41 @@ static int add_redirection(Redirection **redirections,
                            size_t *count,
                            size_t *capacity,
                            RedirectionType type,
-                           const char *filename){
-    if(*count >=*capacity){
-        size_t new_capacity=
-           (*capacity==0)
+                           const char *filename)
+{
+    if (*count >= *capacity) {
+        size_t new_capacity =
+            (*capacity == 0)
                 ? 2
                 : *capacity * 2;
 
-        Redirection *new_redirections=realloc(
-            *redirections,
-            new_capacity * sizeof(Redirection)
-        );
+        Redirection *new_redirections =
+            realloc(*redirections,
+                    new_capacity * sizeof(Redirection));
 
-        if(new_redirections==NULL){
+        if (new_redirections == NULL) {
             return 0;
         }
 
-        *redirections=new_redirections;
-        *capacity=new_capacity;
+        *redirections = new_redirections;
+        *capacity = new_capacity;
     }
 
-   (*redirections)[*count].type=type;
-   (*redirections)[*count].filename=strdup(filename);
+    (*redirections)[*count].type = type;
+    (*redirections)[*count].filename = strdup(filename);
 
-    if((*redirections)[*count].filename==NULL){
+    if ((*redirections)[*count].filename == NULL) {
         return 0;
     }
 
-   (*count)++;
+    (*count)++;
 
     return 1;
 }
 
 static int add_input_redirection(Command *command,
-                                  const char *filename){
+                                 const char *filename)
+{
     return add_redirection(
         &command->input_redirections,
         &command->input_redirection_count,
@@ -209,7 +224,8 @@ static int add_input_redirection(Command *command,
 
 static int add_output_redirection(Command *command,
                                   RedirectionType type,
-                                  const char *filename){
+                                  const char *filename)
+{
     return add_redirection(
         &command->output_redirections,
         &command->output_redirection_count,
@@ -219,130 +235,125 @@ static int add_output_redirection(Command *command,
     );
 }
 
-static int is_redirection(TokenType type){
-    return type==TOKEN_LT ||
-           type==TOKEN_GT ||
-           type==TOKEN_GTGT;
+static int is_redirection(TokenType type)
+{
+    return type == TOKEN_LT ||
+           type == TOKEN_GT ||
+           type == TOKEN_GTGT;
 }
 
 static int parse_redirection(const TokenList *tokens,
                              size_t *index,
-                             Command *command){
-    TokenType operator=tokens->tokens[*index].type;
+                             Command *command)
+{
+    TokenType operator = tokens->tokens[*index].type;
 
-    if(*index + 1 >=tokens->count ||
-        tokens->tokens[*index + 1].type !=TOKEN_WORD){
+    if (*index + 1 >= tokens->count ||
+        tokens->tokens[*index + 1].type != TOKEN_WORD) {
         return 0;
     }
 
-    const char *filename=
+    const char *filename =
         tokens->tokens[*index + 1].value;
 
     int success;
 
-    if(operator==TOKEN_LT){
-        success=add_input_redirection(
-            command,
-            filename
-        );
-    } else if(operator==TOKEN_GT){
-        success=add_output_redirection(
-            command,
-            REDIR_OUTPUT,
-            filename
-        );
-    } else{
-        success=add_output_redirection(
-            command,
-            REDIR_APPEND,
-            filename
-        );
+    if (operator == TOKEN_LT) {
+        success = add_input_redirection(command,
+                                        filename);
+    } else if (operator == TOKEN_GT) {
+        success = add_output_redirection(command,
+                                         REDIR_OUTPUT,
+                                         filename);
+    } else {
+        success = add_output_redirection(command,
+                                         REDIR_APPEND,
+                                         filename);
     }
 
-    if(!success){
+    if (!success) {
         return 0;
     }
 
-    *index +=2;
+    *index += 2;
 
     return 1;
 }
 
 static int parse_pipeline(const TokenList *tokens,
                           size_t *index,
-                          Pipeline *pipeline){
-    if(!add_command(pipeline)){
+                          Pipeline *pipeline)
+{
+    if (!add_command(pipeline)) {
         return 0;
     }
 
-    Command *current=
+    Command *current =
         &pipeline->commands[pipeline->count - 1];
 
-    while(*index < tokens->count){
-        TokenType type=
+    while (*index < tokens->count) {
+        TokenType type =
             tokens->tokens[*index].type;
 
-        if(type==TOKEN_WORD){
-            if(!add_argument(
-                    current,
-                    tokens->tokens[*index].value)){
+        if (type == TOKEN_WORD) {
+            if (!add_argument(current,
+                              tokens->tokens[*index].value)) {
                 return 0;
             }
 
-           (*index)++;
+            (*index)++;
             continue;
         }
 
-        if(is_redirection(type)){
-            if(!parse_redirection(
-                    tokens,
-                    index,
-                    current)){
+        if (is_redirection(type)) {
+            if (!parse_redirection(tokens,
+                                   index,
+                                   current)) {
                 return 0;
             }
 
             continue;
         }
 
-        if(type==TOKEN_PIPE){
-            if(current->argc==0){
+        if (type == TOKEN_PIPE) {
+            if (current->argc == 0) {
                 return 0;
             }
 
-           (*index)++;
+            (*index)++;
 
-            if(*index >=tokens->count ||
-                tokens->tokens[*index].type !=TOKEN_WORD){
+            if (*index >= tokens->count ||
+                tokens->tokens[*index].type != TOKEN_WORD) {
                 return 0;
             }
 
-            if(!add_command(pipeline)){
+            if (!add_command(pipeline)) {
                 return 0;
             }
 
-            current=
+            current =
                 &pipeline->commands[pipeline->count - 1];
 
             continue;
         }
 
-        if(type==TOKEN_AMP){
-            if(current->argc==0){
+        if (type == TOKEN_AMP) {
+            if (current->argc == 0) {
                 return 0;
             }
 
-            pipeline->background=1;
-           (*index)++;
+            pipeline->background = 1;
+            (*index)++;
 
-            if(*index !=tokens->count){
+            if (*index != tokens->count) {
                 return 0;
             }
 
             return 1;
         }
 
-        if(type==TOKEN_SEMI){
-            if(current->argc==0){
+        if (type == TOKEN_SEMI) {
+            if (current->argc == 0) {
                 return 0;
             }
 
@@ -352,64 +363,68 @@ static int parse_pipeline(const TokenList *tokens,
         return 0;
     }
 
-    return current->argc !=0;
+    return current->argc != 0;
 }
 
 ParseResult parse_tokens(const TokenList *tokens,
-                         CommandList *command_list){
-    command_list->pipelines=NULL;
-    command_list->count=0;
-    command_list->capacity=0;
-
-    if(tokens==NULL || tokens->count==0){
+                         CommandList *command_list)
+{
+    if (command_list == NULL) {
         return PARSE_INVALID_SYNTAX;
     }
 
-    size_t index=0;
+    command_list->pipelines = NULL;
+    command_list->count = 0;
+    command_list->capacity = 0;
 
-    while(index < tokens->count){
-        if(tokens->tokens[index].type==TOKEN_SEMI){
+    if (tokens == NULL || tokens->count == 0) {
+        return PARSE_INVALID_SYNTAX;
+    }
+
+    size_t index = 0;
+
+    while (index < tokens->count) {
+        if (tokens->tokens[index].type == TOKEN_SEMI) {
             free_command_list(command_list);
             return PARSE_INVALID_SYNTAX;
         }
 
-        if(!add_pipeline(command_list)){
+        if (!add_pipeline(command_list)) {
             free_command_list(command_list);
             return PARSE_INVALID_SYNTAX;
         }
 
-        Pipeline *pipeline=
+        Pipeline *pipeline =
             &command_list->pipelines[
                 command_list->count - 1
             ];
 
-        if(!parse_pipeline(
-                tokens,
-                &index,
-                pipeline)){
+        if (!parse_pipeline(tokens,
+                            &index,
+                            pipeline)) {
             free_command_list(command_list);
             return PARSE_INVALID_SYNTAX;
         }
 
-        if(index >=tokens->count){
+        if (index >= tokens->count) {
             break;
         }
 
-        if(tokens->tokens[index].type !=TOKEN_SEMI){
+        if (tokens->tokens[index].type != TOKEN_SEMI) {
             free_command_list(command_list);
             return PARSE_INVALID_SYNTAX;
         }
 
         index++;
 
-        if(index >=tokens->count ||
-            tokens->tokens[index].type==TOKEN_SEMI){
+        if (index >= tokens->count ||
+            tokens->tokens[index].type == TOKEN_SEMI) {
             free_command_list(command_list);
             return PARSE_INVALID_SYNTAX;
         }
     }
 
-    if(command_list->count==0){
+    if (command_list->count == 0) {
         free_command_list(command_list);
         return PARSE_INVALID_SYNTAX;
     }
